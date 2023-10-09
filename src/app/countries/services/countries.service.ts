73b4195c76f306753f20bcae71956f8b2fd0bdb0
Laura <laurapicon98@gmail.com>
2023-10-09 21:55:44 +0200

    Contrucción html, ts, servicios e interfaces para formularios reactivos con selector

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Region, SmallCountry } from '../interfaces/country.interfaces';
+import { Observable, of, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+
+  private baseUrl: string = 'https://restcountries.com/v3.1';
+
+  private _regions: Region[] = [ Region.Africa, Region.Americas, Region.Asia, Region.Europe, Region.Oceania ];
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  get regions(): Region[] {
+    return [ ...this._regions ];
+  }
+
+  getCountriesByRegion( region: Region ): Observable <SmallCountry[]> {
+    if ( !region ) return of([]);
+
+    const url: string = `${ this.baseUrl }/region/${ region }?fields=cca3,name,borders`
+
+
+    return this.http.get<SmallCountry[]>(url)
+      .pipe(
+        tap ( response => console.log({ response }) )
+      );
+  }
+
+
+}
